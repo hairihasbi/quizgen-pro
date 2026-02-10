3a1686bda340d4dbeea662543df568326268a0c9
hairihasbi <31791071+hairihasbi@users.noreply.github.com>
2026-02-10 11:41:20 +0800

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     sourcemap: false,
-    minify: 'terser',
+    minify: 'esbuild', // Menggunakan esbuild yang lebih cepat dan bawaan Vite
     chunkSizeWarningLimit: 1600,
   }
 });
